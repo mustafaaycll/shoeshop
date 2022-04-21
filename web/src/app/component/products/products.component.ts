4a2498642a8b9d333a9a9e43fb7985c5aed9ca7f
Yekta Kocaoğullar <ykocaogullar@sabanciuniv.edu>
2022-04-21 23:10:29 +0300

    Added top bar and products to the main page.

---
+++ web/src/app/component/products/products.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/service/api.service';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.scss']
+})
+export class ProductsComponent implements OnInit {
+  public productList : any;
+
+  constructor(private api : ApiService) { }
+
+  ngOnInit(): void {
+    this.api.getProduct()
+    .subscribe(res=>{
+      this.productList = res;
+    })
+  }
+
+}
