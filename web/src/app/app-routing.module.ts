ca8a7fb91adaaf16bce85dfed18bdfd2ad593513
utkukocoglu <utkukocoglu@sabanciuniv.edu>
2022-05-03 14:44:08 +0300

    signup to firebase working

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -3,10 +3,26 @@ import { RouterModule, Routes } from '@angular/router';
 import { CartComponent } from './component/cart/cart.component';
 import { ProductsComponent } from './component/products/products.component';
 
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+import { AuthGuard } from './services/auth.guard';
+//import * as auth0 from "auth0-js";
+
+
+
 const routes: Routes = [
   {path:'', redirectTo:'products',pathMatch:'full'},
   {path:'products', component: ProductsComponent},
-  {path:'cart', component: CartComponent}
+  {path:'cart', component: CartComponent},
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: '**', component: HomeComponent },  
+
 ];
 
 @NgModule({
