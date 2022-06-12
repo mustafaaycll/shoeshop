492b51f360c680bf84097d3526e1ea99d9feded8
Mustafa Ahmet Yücel <mustafayucel@sabanciuniv.edu>
2022-06-12 15:16:15 +0300

    Discount notifier 90% done

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,7 +1,6 @@
 
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-const nodemailer = require("nodemailer");
 admin.initializeApp();
 
 const db = admin.firestore();
@@ -84,3 +83,72 @@ exports.onRequestUpdate = functions.firestore
       }
     });
 
+exports.onNewDiscountApplied = functions.firestore
+    .document("products/{productID}")
+    .onUpdate((change, context) => {
+      const previousValues = change.before.data();
+      const newValues = change.after.data();
+
+      try {
+        if (newValues.discount_rate != previousValues.discount_rate &&
+            newValues.discount_rate != 0 &&
+            newValues.discount_rate > previousValues.discount_rate) {
+          const updatePromises = [];
+          const tos = [];
+          const oldDiscount = previousValues.discount_rate;
+          const newDiscount = newValues.discount_rate;
+          const productID = newValues.id;
+          const productName = newValues.name;
+          const productModel = newValues.model;
+          const productPicture = newValues.photos[0];
+          const oldPrice = (parseFloat(previousValues.price) *
+                            ((100 - oldDiscount) / 100)).toFixed(2);
+          const newPrice = (parseFloat(newValues.price) *
+                            ((100 - newDiscount) / 100)).toFixed(2);
+
+          let email = "";
+          let favList = [];
+          db.collection("customers")
+              .get().then((customerSnapshot) => {
+                customerSnapshot.forEach((doc) => {
+                  email = doc.get("email");
+                  favList = doc.get("fav_products");
+                  if (email != "No Email" &&
+                    favList.length != 0 &&
+                    favList.includes(productID)) {
+                    tos.push(email);
+                  }
+                });
+              });
+          const htmlMessage = '<h1>Time to check your ShoeShop wishlist!</h1>' +
+                              '<p>Apparently we have new discounts for you. ' +
+                              'Check it out.</p>' +
+                              '<p style="text-align: center;">' +
+                              '<img src=' + productPicture +
+                              'alt="productPictiure"' +
+                              'width="240" height="300" /></p>' +
+                              '<h2 style="text-align: center;">' +
+                              productName + ' ' + productModel + ' is now ' +
+                              newDiscount.toString()+'% off</h2>' +
+                              '<h2 style="text-align: center;">' +
+                              newPrice+'₺</h2>'+
+                              '<h3 style="text-align: center;">' +
+                              'instead of '+oldPrice+'₺</h3>';
+
+          if (tos.length != 0) {
+            updatePromises.push(db.collection("mail").doc().set({
+              message: {
+                html: htmlMessage,
+                subject: "New Discount Deal on ShoeShop",
+                text: "",
+              },
+              to: tos,
+            }));
+          }
+
+          return Promise.all(updatePromises);
+        }
+      } catch (err) {
+        return db.collection("logs").doc().set({error: err});
+      }
+    });
